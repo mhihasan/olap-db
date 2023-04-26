9720285a94151bab164d5a3aeefe7806cc3d58e4
mhihasan <hasanuli10@gmail.com>
2023-04-26 19:49:40 +0600

    Bump serp vault

--- rankings_generator.py
+++ rankings_generator.py
@@ -24,7 +24,7 @@ from faker import Faker
 
 fake = Faker()
 
-DEFAULT_PAGE_SIZE = 10000
+DEFAULT_PAGE_SIZE = 1000
 
 import itertools
 from concurrent.futures import FIRST_COMPLETED, wait, ProcessPoolExecutor
