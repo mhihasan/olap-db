611f0efddda1013134a37e445cd780bc40449475
mhihasan <hasanuli10@gmail.com>
2023-06-08 12:15:49 -0400

    Ingest files

--- ingest.py
+++ ingest.py
@@ -6,6 +6,10 @@ import time
 DB_NAME = 'content_inventory'
 TABLE_NAME = 'rankings_v1'
 
+"""
+docker-compose run clickhouse-server clickhouse-client -d content_inventory -h clickhouse-server -q "INSERT INTO rankings_v1 FORMAT CSV" <  rankings_data_en-us/rankings_en-us_1.csv
+"""
+
 
 def ingest(db_name=DB_NAME, table_name=TABLE_NAME):
     for i in range(1, 2138):
@@ -24,6 +28,7 @@ def ingest_file(file_name):
     print(f'Finished in {time.perf_counter() - t1} seconds')
 
 
+
 def cli():
     parser = argparse.ArgumentParser()
     parser.add_argument('--db', type=str, default=DB_NAME)
@@ -34,8 +39,11 @@ def cli():
 
 
 if __name__ == '__main__':
-    folder_names = ['rankings_data_en-us', 'rankings_data_en-uk', 'rankings_data']
-    for folder_name in folder_names:
-        files = os.listdir(folder_name)
-        for f in files:
-            ingest_file(f'{folder_name}/{f}')
+    with open('ingest.sh', 'w+') as file_writer:
+        file_writer.write("#!/usr/bin/env bash")
+        file_writer.write("set -e")
+        folder_names = ['rankings_data_en-us', 'rankings_data_en-uk', 'rankings_data']
+        for folder_name in folder_names:
+            files = os.listdir(folder_name)
+            for f in files:
+                file_writer.write(f'docker-compose run clickhouse-server clickhouse-client -d {DB_NAME} -h clickhouse-server -q "INSERT INTO {TABLE_NAME} FORMAT CSV" < {folder_name}/{f}')
